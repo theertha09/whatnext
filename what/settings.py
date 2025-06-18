336fb7194e21c5f8412f1140fbe838ce309a2450
theertha09 <161118217+theertha09@users.noreply.github.com>
2025-06-18 11:16:03 +0530

    ,

--- what/settings.py
+++ what/settings.py
@@ -65,7 +65,7 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "http://localhost:3001"
+    "http://localhost:3001",
     "https://what-next-beta.vercel.app"
 ]
 
